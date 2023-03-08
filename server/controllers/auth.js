891a0c302693ce64e0e51ffc87f66300591ccb1f
zahid-soft <zahidsarang95@gmail.com>
2023-03-08 19:20:00 +0530

    Authentication and Authorization is done

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import Jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 /* REGISTER USER */
@@ -22,7 +22,7 @@ export const register = async (req, res) => {
       firstName,
       lastName,
       email,
-      pasword: passwordHash,
+      password: passwordHash,
       picturePath,
       friends,
       location,
@@ -33,6 +33,22 @@ export const register = async (req, res) => {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
   } catch (err) {
-    res.status(500).json({error:err.message});
+    res.status(500).json({ error: err.message });
+  }
+};
+
+/* LOGGING IN */
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist." });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+    const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
+    delete user.password;
+    res.status(200).json({token,user});
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 };
