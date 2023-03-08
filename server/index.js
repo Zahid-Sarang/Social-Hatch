78a77e7bdeb80cb7f29ee744aa6587e259d2be9c
zahid-soft <zahidsarang95@gmail.com>
2023-03-08 22:45:21 +0530

    last comments

--- server/index.js
+++ server/index.js
@@ -10,10 +10,14 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/user.js";
-import postsRoutes from "./routes/posts.js"
+import postsRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
-import { createPost } from "./controllers/posts.js"
+import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users,posts } from "./data/index.js"
+
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -42,12 +46,12 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
-app.post("/posts",verifyToken,upload.single("picture"),createPost)
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
-app.use("/posts",postsRoutes);
+app.use("/posts", postsRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 8000;
@@ -58,5 +62,9 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Running On Port:${PORT}`));
+     
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
