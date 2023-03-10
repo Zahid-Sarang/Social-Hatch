5d0366287a57eb49cd60a95e961cd9f42cff5ee3
zahid-soft <zahidsarang95@gmail.com>
2023-03-10 14:47:27 +0530

    Routes Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
-
+import { useState } from "react";
+import { BrowserRouter, Navogate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homepage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
 function App() {
+  const mode = useState();
   return (
     <div className="App">
-     
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
