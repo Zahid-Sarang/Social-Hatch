376d21ad652f3756d1c645f28ba22ea83fba3a78
zahid-soft <zahidsarang95@gmail.com>
2023-03-14 22:29:35 +0530

    Home Page Profile Section

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "public/assests")));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
@@ -50,7 +50,7 @@ app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
-app.use("/user", userRoutes);
+app.use("/users", userRoutes);
 app.use("/posts", postsRoutes);
 
 /* MONGOOSE SETUP */
